46e5afb8b55347410e274f4f3fa03be9d93e06d4
GoRoSfan <olsngrs@gmail.com>
2019-09-13 19:54:51 +0300

    Some view commit

--- public/views.py
+++ public/views.py
@@ -4,8 +4,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions
 
-from .models import ContentTypesNews, News
-from .serializers import AllNewsSerializers
+from .models import News, Contacts, Legals, Clubs, TrainingCourses, Events
+from .serializers import AllNewsSerializers, LegalsSerializers, ContactsSerializers, AllClubsSerializers,\
+    AllTrainingCoursesSerializers, AllEventsSerializers
 
 # Create your views here.
 
@@ -16,3 +17,30 @@ class AllNewsView(APIView):
         news = News.objects.all()
         serializer = AllNewsSerializers(news, many=True)
         return Response({'data': serializer.data})
+
+
+class LegalsView(APIView):
+
+    def get(self, request):
+        legals = Legals.objects.all()
+        serializer = LegalsSerializers(legals, many=True)
+        return Response({'data': serializer.data})
+
+
+class ContactsView(APIView):
+
+    def get(self, request):
+        contacts = Contacts.objects.all()
+        serializer = ContactsSerializers(contacts, many=True)
+        return Response({'data': serializer.data})
+
+
+class AllClubsView(APIView):
+
+    def get(self, request):
+        clubs = Clubs.objects.all()
+        training_courses = TrainingCourses.objects.all()
+        serializer_clubs = AllClubsSerializers(clubs, many=True)
+        serializer_courses = AllTrainingCoursesSerializers(training_courses, many=True)
+        serializer = {'serializer_courses': serializer_courses.data, 'serializer_clubs': serializer_clubs}
+        return Response({'data': serializer})
