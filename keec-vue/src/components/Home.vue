da534a99179677a492cc57d056fcd6d90cbe3d5e
GoRoSfan <olsngrs@gmail.com>
2019-09-21 19:42:44 +0300

    Just

--- keec-vue/src/components/Home.vue
+++ keec-vue/src/components/Home.vue
@@ -1,17 +1,25 @@
 <template>
   <mu-container class="main-container">
-    <mu-appbar class="header" color="primary"></mu-appbar>
+    <mu-col span="12" justify-content="center">text text text text text text text text</mu-col>
     <mu-appbar class="header" color="primary">
-      <mu-col span="3" class="mu-menu m">Новини</mu-col>
-      <mu-col span="3" class="mu-menu m">План роботи</mu-col>
-      <mu-col span="3" class="mu-menu m">Додатково</mu-col>
+      <mu-flex class="menu-list">
+        <mu-flex justify-content="center" class="menu-item" fill>
+          <mu-menu class="">
+            Новини
+          </mu-menu>
+        </mu-flex>
+        <mu-flex justify-content="center" class="menu-item" fill>План роботи</mu-flex>
+        <mu-flex justify-content="center" class="menu-item" fill>Додатково</mu-flex>
+      </mu-flex>
     </mu-appbar>
-    <mu-row gutter>
-      <mu-col span="9" class="main-container" color="primary"><div class="s"></div></mu-col>
-      <mu-col span="3" class="sidebar s" color="primary">
-        <mu-row class="last-news" color="primary s"></mu-row>
-        <mu-row class="helpful-links" color="primary"></mu-row>
-      </mu-col>
+    <mu-row>
+      <mu-col span="9" class="main-container s" color="primary"></mu-col>
+      <mu-flex class="" direction="row-reverse" fill>
+        <mu-col span="9" class="sidebar s" color="primary" align-self="end">
+          <mu-row class="last-news" color="primary"></mu-row>
+          <mu-row class="helpful-links" color="primary"></mu-row>
+        </mu-col>
+      </mu-flex>
     </mu-row>
   </mu-container>
 </template>
@@ -27,6 +35,15 @@ export default {
     border: black solid 3px;
     height: 400px;
     background-color: cyan ;
+  }
+
+  .sidebar{
+    height: 150px;
+    justify-self: right;
+  }
+
+  .menu-item{
+    line-height: 1.2;
   }
   /*.main-container{*/
     /*margin: 0px;*/
