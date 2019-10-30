088d427777fe93773f681764f06e84ba3bd50f7f
GoRoSfan <olsngrs@gmail.com>
2019-10-30 19:06:37 +0200

    recommit

--- keec-vue/src/components/About.vue
+++ keec-vue/src/components/About.vue
@@ -1,10 +1,24 @@
 <template>
-  <div></div>
+  <HomeSlot #main>
+    <div class="context-header"><h2>Про нас</h2></div>
+
+  </HomeSlot>
 </template>
 
 <script>
+  import HomeSlot from './Home';
+
+
 export default {
   name: 'About',
+
+  components: {
+    HomeSlot,
+  },
+
+  data: function () {
+    return {}
+  },
 };
 </script>
 
